82b89adf87166c7a6a235cad9076dc7624f7ad49
deedan99 <50495306+deedan99@users.noreply.github.com>
2021-09-30 23:28:13 +0700

    Add files via upload

---
+++ GameOverManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour{
+    public PlayerHealth playerHealth;
+	public Text warningText;
+    public float restartDelay = 5f;   
+	public float enemyDistance;
+    Animator anim;                          
+    float restartTimer;                    
+    void Awake(){
+        anim = GetComponent<Animator>();
+    }
+    void Update(){
+        if (playerHealth.currentHealth <= 0){
+            anim.SetTrigger("GameOver");
+            restartTimer += Time.deltaTime;
+            if (restartTimer >= restartDelay){
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+	public void ShowWarning(){
+		warningText.text = string.Format("! {0} m",Mathf.RoundToInt(enemyDistance));
+        anim.SetTrigger("Warning");
+    }
+}
---
+++ IFactory.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IFactory{
+    GameObject FactoryMethod(int tag);
+}
